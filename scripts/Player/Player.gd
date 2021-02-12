cfd622f53efec0135a36e54da4b7ef956dddece7
fruitdude <stevenrogge95@gmail.com>
2021-02-12 11:55:03 +0100

    animation tree & logic for movement

--- scripts/Player/Player.gd
+++ scripts/Player/Player.gd
@@ -2,12 +2,17 @@ extends KinematicBody
 
 
 const SPEED : float = 7.5
+const MIN_BLEND_SPEED : float = 0.125
+const BLEND_TO_RUN : float = 0.075
+const BLEND_TO_IDLE : float = 0.1
 
 var _motion : Vector3 = Vector3()
+var _movement_state : float = 0 #idle is 0, run is 1
 
 
 func _physics_process(delta):
 	_move()
+	_animate()
 	
 	
 func _move():
@@ -23,4 +28,15 @@ func _move():
 	
 	
 func face_forward(x, z):
-	rotation.y = atan2(x,z) - PI/2
+	rotation.y = atan2(x,z)
+	
+	
+func _animate():
+	if (_motion * SPEED).length() > MIN_BLEND_SPEED:
+		_movement_state += BLEND_TO_RUN
+	else:
+		_movement_state -= BLEND_TO_IDLE
+		
+	_movement_state = clamp(_movement_state, 0, 1)
+	var anim_tree = $Armature/AnimationTree
+	anim_tree["parameters/Move/blend_amount"] = _movement_state	
